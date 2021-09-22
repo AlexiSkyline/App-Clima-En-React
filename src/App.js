774a16c05aa16264b5c20b062296a51d7c609f08
AlexiSkyline <ilegal_sprite@outlook.com>
2021-09-22 11:52:47 -0500

    Creamos nuestro componente Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Header } from './components/Header';
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>Clima React</h1>
-    </div>
+    <>
+      <Header 
+        titulo="Clima React App"
+      />
+
+      <div className="contenedor-form">
+          <div className="container">
+              <div className="row">
+                  <div className="col m6 s12">
+                    1
+                  </div>
+                  <div className="col m6 s12">
+                    2
+                  </div>
+              </div>
+          </div>
+      </div>
+    </>
   );
 }
 
